391177c163652c93dbe6c4339b73cbf662d0b9d6
= <daniel.hartman@mymail.champlain.edu>
2021-02-03 17:18:20 -0500

    Raknet tutorial thing done

--- GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
@@ -22,6 +22,8 @@
 	Main source for console client application.
 */
 
+//using namespace RakNet;
+
 #include "gpro-net/gpro-net.h"
 
 
@@ -30,13 +32,109 @@
 #include <string.h>
 
 
+//using namespace RakNet;
+
+#include "Raknet/MessageIdentifiers.h"
 #include "RakNet/RakPeerInterface.h"
+#include "RakNet/RakNetSocket.h"
+#include "RakNet/SocketIncludes.h"
+#include "RakNet/BitStream.h"
+#include "RakNet/RakNetTypes.h"  // MessageID
+
+#define MAX_CLIENTS 10
+#define SERVER_PORT 4024
+
+enum GameMessages
+{
+	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1
+};
 
 
 int main(int const argc, char const* const argv[])
 {
+	//char str[512];
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+
+	
+	RakNet::SocketDescriptor sd;
+	peer->Startup(1, &sd, 1);
+	isServer = false;
+	RakNet::Packet* packet;
 
 
+	// TODO - Add code body here
+
+	
+	
+	printf("Starting the client.\n");
+	peer->Connect("172.16.2.186", SERVER_PORT, 0, 0);
+
+	
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+			{
+				printf("Our connection request has been accepted.\n");
+
+				// Use a BitStream to write a custom user message
+				// Bitstreams are easier to use than sending casted structures, and handle endian swapping automatically
+				RakNet::BitStream bsOut;
+				bsOut.Write((RakNet::MessageID)ID_GAME_MESSAGE_1);
+				bsOut.Write("Hello world");
+				peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 0, packet->systemAddress, false);
+			}
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+					printf("We have been disconnected.\n");
+				break;
+			case ID_CONNECTION_LOST:
+					printf("Connection lost.\n");
+				break;
+			case ID_GAME_MESSAGE_1:
+			{
+				RakNet::RakString rs;
+				RakNet::BitStream bsIn(packet->data, packet->length, false);
+				bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+				bsIn.Read(rs);
+				printf("%s\n", rs.C_String());
+			}
+			case ID_CONNECTION_ATTEMPT_FAILED:
+					printf("Connection attempt failed.\n");
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
+
+
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
+
+	return 0;
+	printf("test message");
 	printf("\n\n");
 	system("pause");
 }
