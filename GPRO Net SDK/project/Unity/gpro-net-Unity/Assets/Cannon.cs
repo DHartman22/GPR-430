dd0c8968910276b9963c86f9463b9ef19ec1a6db
Daniel Hartman <daniel.hman123@gmail.com>
2021-04-21 17:58:26 -0400

    Adding previously missing unity files

---
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Cannon.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public GameObject cannonBallPrefab;
+    public float shotPower;
+    public GameObject cannonModel;
+    void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        GameObject shot = Instantiate(cannonBallPrefab, transform.position, Quaternion.identity);
+        //shot.transform.parent = null;
+        //shot.transform.localScale = Vector3.one;
+        Ray trajectory = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(trajectory, out RaycastHit hit))
+        {
+            Debug.DrawRay(trajectory.origin, trajectory.direction, Color.red);
+            Vector3 shootDirection = hit.point - transform.position;
+            shootDirection.Normalize();
+
+            shot.GetComponent<Rigidbody>().velocity = shootDirection * shotPower;
+        }
+        
+        
+    }
+
+    void AimCannon()
+    {
+        //GameObject shot = Instantiate(cannonBallPrefab, transform);
+        //shot.transform.parent = null;
+        //shot.transform.localScale = Vector3.one;
+        Ray trajectory = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(trajectory, out RaycastHit hit))
+        {
+            Debug.DrawLine(trajectory.origin, trajectory.direction, Color.red);
+            Vector3 shootDirection = hit.point - transform.position;
+            shootDirection.Normalize();
+            cannonModel.transform.LookAt(hit.point);
+            //Vector3.
+            //shot.GetComponent<Rigidbody>().velocity = shootDirection * shotPower;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        AimCannon();
+    }
+}
---
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/PlayerMovement.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float xMove = 0f;
+    float zMove = 0f;
+    Vector3 movement;
+    public float speed = 15f;
+    public float runSpeedBoost = 3f;
+    public float stompWindow; //Meant to prevent stomps while rising
+    public float stompVelocity;
+    public CharacterController characterController;
+
+    //gravity
+    public Vector3 velocity;
+    public float gravity = -9.8f;
+    public Transform groundCheck;
+    public Transform bonkCheck;
+    bool isGrounded;
+    public LayerMask groundMask;
+    public LayerMask bouncyMask;
+    float groundCheckDistance = 0.5f;
+
+    public float jumpHeight = 5f;
+    public bool inputAllowed = true;
+    public bool gravityPaused = false;
+
+    private AudioSource bounce;
+
+    void Move()
+    {
+        if (inputAllowed)
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -1;
+            }
+
+            if (gravityPaused == false)
+            {
+                velocity.y += gravity * Time.deltaTime;
+                characterController.Move(velocity * Time.deltaTime);
+            }
+
+            xMove = Input.GetAxis("Horizontal");
+
+            zMove = Input.GetAxis("Vertical");
+
+            movement = transform.right * xMove + transform.forward * zMove;
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                characterController.Move(movement * (speed + runSpeedBoost) * Time.deltaTime);
+            }
+            else
+            {
+                characterController.Move(movement * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Trampoline")
+        {
+            Bounce();
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+
+    public void Bounce()
+    {
+        if (Physics.CheckSphere(groundCheck.position, groundCheckDistance, bouncyMask))
+        {
+            if (velocity.y > (Mathf.Sqrt((jumpHeight * 1.5f) * -2 * gravity) * -1) - 0.1f) //Prevents idle bouncing from triggering the bigger bounce effects 
+            {
+                velocity.y = Mathf.Sqrt((jumpHeight * 1.5f) * -2 * gravity);
+                bounce.pitch = 1;
+                bounce.Play();
+            }
+            else
+            {
+                velocity.y *= -1;
+                bounce.pitch = 2;
+                bounce.Play();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounce = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputAllowed)
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+}
