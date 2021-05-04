a1ff49ce090df95265f057608b5d9fff053ff97e
Daniel Hartman <daniel.hman123@gmail.com>
2021-05-04 02:40:04 -0400

    Dead reckoning and interpolation attempts

--- GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Puck.cs
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Puck.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Puck : MonoBehaviour
 {
+    public Vector3 vel;
+
     private Rigidbody rgd;
     public float maxSpeed;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 11)
         {
-            //GetComponent<Rigidbody>().velocity *= 1.2f;
+            GetComponent<Rigidbody>().velocity *= 1.2f;
         }
     }
     // Start is called before the first frame update
@@ -22,7 +24,32 @@ public class Puck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(rgd.velocity.magnitude);
-        Vector3.ClampMagnitude(rgd.velocity, maxSpeed);
+        //rgd.velocity = Vector3.ClampMagnitude(rgd.velocity, maxSpeed);
+        if(rgd.velocity.x > maxSpeed)
+        {
+            
+            
+            rgd.velocity = new Vector3(maxSpeed, 0f, rgd.velocity.z);
+            Debug.Log("Slowing X");
+        }
+
+        if (rgd.velocity.x < maxSpeed * -1)
+        {
+            rgd.velocity = new Vector3(maxSpeed * -1, 0f, rgd.velocity.z);
+            Debug.Log("Slowing X");
+        }
+
+        if (rgd.velocity.z > maxSpeed)
+        {
+            rgd.velocity = new Vector3(rgd.velocity.x, 0f, maxSpeed);
+            Debug.Log("Slowing Z");
+        }
+
+        if (rgd.velocity.z < maxSpeed * -1)
+        {
+            rgd.velocity = new Vector3(rgd.velocity.x, 0f, maxSpeed);
+            Debug.Log("Slowing Z");
+        }
+        vel = rgd.velocity;
     }
 }
